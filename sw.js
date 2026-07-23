94f9b93f59829bca6ded65a2a5e759818019de14
jhonnmv19 <ajhonn17mv@gmail.com>
2026-07-23 09:11:19 -0400

    avances del dashboard

--- sw.js
+++ sw.js
@@ -2,41 +2,41 @@ const CACHE_NAME = 'surtidor-pwa-v1';
 
 // Lista de assets locales que la PWA guardará en caché
 const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './css/output.css',
-  './css/input.css',
-  './config/supabase.js',
+  '/',
+  '/index.html',
+  '/css/output.css',
+  '/css/input.css',
+  '/config/supabase.js',
   
   // Controladores y Modelos
-  './controllers/mainController.js',
-  './controllers/authController.js',
-  './controllers/alertaController.js',
-  './controllers/surtidorController.js',
-  './controllers/tanqueController.js',
-  './controllers/ventaController.js',
-  './models/authModel.js',
-  './models/alertaModel.js',
-  './models/surtidorModel.js',
-  './models/tanqueModel.js',
-  './models/ventaModel.js',
-  './models/reporteModel.js',
+  '/controllers/mainController.js',
+  '/controllers/authController.js',
+  '/controllers/alertaController.js',
+  '/controllers/surtidorController.js',
+  '/controllers/tanqueController.js',
+  '/controllers/ventaController.js',
+  '/models/authModel.js',
+  '/models/alertaModel.js',
+  '/models/surtidorModel.js',
+  '/models/tanqueModel.js',
+  '/models/ventaModel.js',
+  '/models/reporteModel.js',
   
   // Vistas y Componentes
-  './views/dashboardView.js',
-  './views/surtidoresView.js',
-  './views/tanquesView.js',
-  './views/ventasView.js',
-  './views/reportesView.js',
-  './views/componentes/navbar.js',
-  './views/componentes/sidebar.js',
+  '/views/dashboardView.js',
+  '/views/surtidoresView.js',
+  '/views/tanquesView.js',
+  '/views/ventasView.js',
+  '/views/reportesView.js',
+  '/views/componentes/navbar.js',
+  '/views/componentes/sidebar.js',
   
   // Imágenes e Íconos
-  './imagen/yautja1.png',
-  './imagen/yautja2.png',
-  './imagen/daboarh.png',
-  './imagen/captabla.png',
-  './imagen/creacion_de_tablas.png'
+  '/imagen/yautja1.png',
+  '/imagen/yautja2.png',
+  '/imagen/daboarh.png',
+  '/imagen/captabla.png',
+  '/imagen/creacion_de_tablas.png'
 ];
 
 // Instalación: Guarda todos los archivos iniciales en caché
